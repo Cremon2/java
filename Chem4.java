d0b3e9574bd90aa75d8f039ebb3dde92b7eb1f10
Cremon2 <103885151+Cremon2@users.noreply.github.com>
2022-06-08 16:00:00 +0800

    Update Chem4.java

--- Chem4.java
+++ Chem4.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Chem4;
 
-/**
- *
- * @author sheilo roxas
- */
+
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
